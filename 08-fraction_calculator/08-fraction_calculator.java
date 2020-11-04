ec4d74a7fd29333834332f0ed6f05f6c835e3ac3
Joshua <jsiganur@gmail.com>
2020-11-04 17:52:00 -0500

    did part of checkpoint 2

--- 08-fraction_calculator/08-fraction_calculator.java
+++ 08-fraction_calculator/08-fraction_calculator.java
@@ -4,10 +4,20 @@ class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
-    System.out.println("Please enter an equation in the form of operand, operator, operand");
+    boolean quit = true;
+
+    do {
+
+    System.out.println("Please enter an equation in the form of operand, operator, operand. If you want to quit, just type quit.");
+
     String fracIn = in.nextLine();
 
+    if (fracIn == "quit") {
+      quit = false;
+    }
     produceAnswer(fracIn);
+
+    } while (quit);
   }
 
   public static String produceAnswer(String equation){
@@ -19,6 +29,19 @@ class Main {
     String secOperand = equation.substring((firstBlank + 3), len);
 
     System.out.println(secOperand);
+
+    boolean conSlash = secOperand.contains("/");
+    boolean conUnder = secOperand.contains("_");
+
+    if (conSlash == true) {
+      if (conUnder == true) {
+        System.out.println("shmoop");
+      }
+      System.out.println("shmoop");
+    }
+
+
+
     return (secOperand);
   }
 }
