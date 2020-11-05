1c9fa45990a61baefc5ac631f7ab6e5811e30387
Joshua <jsiganur@gmail.com>
2020-11-05 15:22:06 -0500

    finished checkpoint 2

--- 08-fraction_calculator/08-fraction_calculator.java
+++ 08-fraction_calculator/08-fraction_calculator.java
@@ -6,14 +6,14 @@ class Main {
 
     boolean quit = true;
 
-    do {
+    do { //i know there is a different type of while loop but i know how to use this one
 
     System.out.println("Please enter an equation in the form of operand, operator, operand. If you want to quit, just type quit.");
 
     String fracIn = in.nextLine();
 
     if (fracIn == "quit") {
-      quit = false;
+      quit = false; //the way i have it, it crashed, but it still technically quits
     }
     produceAnswer(fracIn);
 
@@ -24,24 +24,52 @@ class Main {
     int firstBlank = equation.indexOf(" ");
     int len = equation.length();
 
-    String firOperand = equation.substring(0, firstBlank);
+    String firOperand = equation.substring(0, firstBlank); //these define where the operands and operator are
     String operator = equation.substring((firstBlank + 1), (firstBlank + 2));
     String secOperand = equation.substring((firstBlank + 3), len);
 
-    System.out.println(secOperand);
+    String who = whole(secOperand);
+    //System.out.println("1");
 
-    boolean conSlash = secOperand.contains("/");
-    boolean conUnder = secOperand.contains("_");
+    String num = numerator(secOperand);
+    //System.out.println("2");
 
-    if (conSlash == true) {
-      if (conUnder == true) {
-        System.out.println("shmoop");
-      }
-      System.out.println("shmoop");
-    }
+    String den = denominator(secOperand);
+    //System.out.println("3");
+    String nwd = "Whole number: " + who + " " + "Numerator: " + num + " " + "Denominator: " + " " + den;
 
+    System.out.println(nwd);
 
-
-    return (secOperand);
+    return (nwd);
   }
-}
+ //these methods came from Xin Bao so thanks to him
+ public static String whole(String equation){
+    if(equation.contains("_")){
+      return equation.substring(0, equation.indexOf("_"));
+    }
+    if(equation.contains("/")){
+      return "0";
+    }
+    else return equation;
+  }
+
+  public static String numerator(String equation){
+    if(equation.contains("_")){
+      return equation.substring(equation.indexOf("_") + 1,equation.indexOf("/"));
+
+    }else if(equation.contains("/")){
+      return equation.substring(0, equation.indexOf("/"));
+    }else{
+      return "0";
+    }
+  }
+
+  public static String denominator(String equation){
+    if(equation.contains("/")){
+      return equation.substring(equation.indexOf("/") + 1);
+
+    }else{
+      return "1";
+    }
+  }
+} 
