dacf686366f80e9e24be0968bbfab984076f551a
Joshua <jsiganur@gmail.com>
2020-12-02 17:11:58 -0500

    added plan for 2020 day 2

---
+++ 09-advent/2020day2.java
@@ -0,0 +1,12 @@
+class twentytwentydaytwo {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+/*
+What Does the Code Need to do?
+The code needs to be able to take in an input that is in a text file, separate each input into multiple parts, and evaluate.
+
+How the Code Does it.
+A method is made that separates each input value into three parts: the amount of times a letter should appear, the letter, and the string of letters. Then, a larger method takes the parts, and begins to evaluate the string. Variables are made that determine amount of times a letter appears (like a minimum and maximum), the letter, and the string of letters. If the method sees the letter more or less than however many times it's supposed to be there, it doesn't get accounted for, otherwise it does.
+*/
